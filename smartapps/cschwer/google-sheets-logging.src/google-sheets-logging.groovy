653b9e7ad248e68928b68e65975fd0378a318a4e
John LoVerso <john.loverso@gmail.com>
2016-06-07 15:14:41 -0400

    Normalize whitespace

--- smartapps/cschwer/google-sheets-logging.src/google-sheets-logging.groovy
+++ smartapps/cschwer/google-sheets-logging.src/google-sheets-logging.groovy
@@ -15,20 +15,20 @@
  */
 
 definition(
-		name: "Google Sheets Logging",
-		namespace: "cschwer",
-		author: "Charles Schwer",
-		description: "Log to Google Sheets",
-		category: "My Apps",
-		iconUrl: "https://raw.githubusercontent.com/loverso-smartthings/googleDocsLogging/master/img/logoSheets.png",
-		iconX2Url: "https://raw.githubusercontent.com/loverso-smartthings/googleDocsLogging/master/img/logoSheets@2x.png",
-		iconX3Url: "https://raw.githubusercontent.com/loverso-smartthings/googleDocsLogging/master/img/logoSheets@2x.png")
+        name: "Google Sheets Logging",
+        namespace: "cschwer",
+        author: "Charles Schwer",
+        description: "Log to Google Sheets",
+        category: "My Apps",
+        iconUrl: "https://raw.githubusercontent.com/loverso-smartthings/googleDocsLogging/master/img/logoSheets.png",
+        iconX2Url: "https://raw.githubusercontent.com/loverso-smartthings/googleDocsLogging/master/img/logoSheets@2x.png",
+        iconX3Url: "https://raw.githubusercontent.com/loverso-smartthings/googleDocsLogging/master/img/logoSheets@2x.png")
 
 preferences {
     section("Contact Sensors to Log") {
-		input "contacts", "capability.contactSensor", title: "Doors open/close", required: false, multiple: true
+        input "contacts", "capability.contactSensor", title: "Doors open/close", required: false, multiple: true
         input "contactLogType", "enum", title: "Value to log", options: ["open/closed", "true/false", "1/0"], defaultValue: "open/closed", required: true, multiple: false
-	}
+    }
     
     section("Motion Sensors to Log") {
         input "motions", "capability.motionSensor", title: "Motion Sensors", required: false, multiple: true
@@ -40,15 +40,15 @@ preferences {
         input "coolingSetPoints", "capability.thermostat", title: "Cooling Setpoints", required: false, multiple: true
         input "thermOperatingStates", "capability.thermostat", title: "Operating States", required: false, multiple: true
     }
-	
+    
     section("Locks to Log") {
-    	input "locks", "capability.lock", title: "Locks", multiple: true, required: false
+        input "locks", "capability.lock", title: "Locks", multiple: true, required: false
         input "lockLogType", "enum", title: "Value to log", options: ["locked/unlocked", "true/false", "1/0"], defaultValue: "locked/unlocked", required: true, multiple: false
-	}
+    }
     
     section("Log Other Devices") {
-    	input "batteries", "capability.battery", title: "Batteries", multiple: true, required: false
-		input "temperatures", "capability.temperatureMeasurement", title: "Temperatures", required:false, multiple: true
+        input "batteries", "capability.battery", title: "Batteries", multiple: true, required: false
+        input "temperatures", "capability.temperatureMeasurement", title: "Temperatures", required:false, multiple: true
         input "energyMeters", "capability.energyMeter", title: "Energy Meters", required: false, multiple: true
         input "powerMeters", "capability.powerMeter", title: "Power Meters", required: false, multiple: true
         input "humidities", "capability.relativeHumidityMeasurement", title: "Humidity Sensors", required: false, multiple: true
@@ -57,11 +57,11 @@ preferences {
         input "switches", "capability.switch", title: "Switches", required: false, multiple: true
         input "sensors", "capability.sensor", title: "Sensors", required: false, multiple: true
         input "sensorAttributes", "text", title: "Sensor Attributes (comma delimited)", required: false
-	}
+    }
 
-	section ("Google Sheets script url key...") {
-		input "urlKey", "text", title: "URL key"
-	}
+    section ("Google Sheets script url key...") {
+        input "urlKey", "text", title: "URL key"
+    }
     
     section ("Technical settings") {
         input "queueTime", "enum", title:"Time to queue events before pushing to Google (in minutes)", options: ["0", "5", "10", "15"], defaultValue:"5"
@@ -70,47 +70,47 @@ preferences {
 }
 
 def installed() {
-	setOriginalState()
-	initialize()
+    setOriginalState()
+    initialize()
 }
 
 def updated() {
-	log.debug "Updated"
-	unsubscribe()
-	initialize()
-    if(settings.resetVals == "yes") {
-    	setOriginalState()
+    log.debug "Updated"
+    unsubscribe()
+    initialize()
+    if (settings.resetVals == "yes") {
+        setOriginalState()
         settings.resetVals = "no"
     }
 }
 
 def initialize() {
-	log.debug "Initialized"
+    log.debug "Initialized"
     subscribe(locks, "lock", handleLockEvent)
-	subscribe(batteries, "battery", handleNumberEvent)
-	subscribe(contacts, "contact", handleContactEvent)
-	subscribe(motions, "motion", handleMotionEvent)
-	subscribe(heatingSetPoints, "heatingSetpoint", handleNumberEvent)
-	subscribe(coolingSetPoints, "coolingSetpoint", handleNumberEvent)
-	subscribe(thermOperatingStates, "thermostatOperatingState", handleStringEvent)
-	subscribe(temperatures, "temperature", handleNumberEvent)
-	subscribe(energyMeters, "energy", handleNumberEvent)
-	subscribe(powerMeters, "power", handleNumberEvent)
-	subscribe(humidities, "humidity", handleNumberEvent)
-	subscribe(illuminances, "illuminance", handleNumberEvent)
-	subscribe(presenceSensors, "presence", handleStringEvent)
-	subscribe(switches, "switch", handleStringEvent)
-	if (sensors != null && sensorAttributes != null) {
-		sensorAttributes.tokenize(',').each {
-			subscribe(sensors, it, handleStringEvent)
-			}
-	}
+    subscribe(batteries, "battery", handleNumberEvent)
+    subscribe(contacts, "contact", handleContactEvent)
+    subscribe(motions, "motion", handleMotionEvent)
+    subscribe(heatingSetPoints, "heatingSetpoint", handleNumberEvent)
+    subscribe(coolingSetPoints, "coolingSetpoint", handleNumberEvent)
+    subscribe(thermOperatingStates, "thermostatOperatingState", handleStringEvent)
+    subscribe(temperatures, "temperature", handleNumberEvent)
+    subscribe(energyMeters, "energy", handleNumberEvent)
+    subscribe(powerMeters, "power", handleNumberEvent)
+    subscribe(humidities, "humidity", handleNumberEvent)
+    subscribe(illuminances, "illuminance", handleNumberEvent)
+    subscribe(presenceSensors, "presence", handleStringEvent)
+    subscribe(switches, "switch", handleStringEvent)
+    if (sensors != null && sensorAttributes != null) {
+        sensorAttributes.tokenize(',').each {
+            subscribe(sensors, it, handleStringEvent)
+            }
+    }
 }
 
 def setOriginalState() {
-	log.debug "Set original state"
-	unschedule()
-	atomicState.queue = [:]
+    log.debug "Set original state"
+    unschedule()
+    atomicState.queue = [:]
     atomicState.failureCount=0
     atomicState.scheduled=false
     atomicState.lastSchedule=0
@@ -118,135 +118,135 @@ def setOriginalState() {
 
 def handleStringEvent(evt) {
 log.debug "handling string event ${evt}"
-	if(settings.queueTime.toInteger() > 0) {
-    	queueValue(evt) { it }
+    if (settings.queueTime.toInteger() > 0) {
+        queueValue(evt) { it }
     } else {
-    	sendValue(evt) { it }
+        sendValue(evt) { it }
     }
 }
 
 def handleNumberEvent(evt) {
-	if(settings.queueTime.toInteger() > 0) {
-    	queueValue(evt) { it.toString() }
+    if (settings.queueTime.toInteger() > 0) {
+        queueValue(evt) { it.toString() }
     } else {
-    	sendValue(evt) { it.toString() }
+        sendValue(evt) { it.toString() }
     }
 }
 
 def handleContactEvent(evt) {
-	// default to open/close, the value of the event
+    // default to open/close, the value of the event
     def convertClosure = { it }
     if (contactLogType == "true/false")
-    	convertClosure = { it == "open" ? "true" : "false" }
+        convertClosure = { it == "open" ? "true" : "false" }
     else if ( contactLogType == "1/0")
-    	convertClosure = { it == "open" ? "1" : "0" }
+        convertClosure = { it == "open" ? "1" : "0" }
 
-	if(settings.queueTime.toInteger() > 0) {
-		queueValue(evt, convertClosure)
+    if (settings.queueTime.toInteger() > 0) {
+        queueValue(evt, convertClosure)
     } else {
-		sendValue(evt, convertClosure)
+        sendValue(evt, convertClosure)
     }
 }
 
 def handleMotionEvent(evt) {
-	// default to active/inactive, the value of the event
+    // default to active/inactive, the value of the event
     def convertClosure = { it }
     if (motionLogType == "true/false")
-    	convertClosure = { it == "active" ? "true" : "false" }
+        convertClosure = { it == "active" ? "true" : "false" }
     else if (motionLogType == "1/0")
-    	convertClosure = { it == "active" ? "1" : "0" }
+        convertClosure = { it == "active" ? "1" : "0" }
 
-	if(settings.queueTime.toInteger() > 0) {
-    	queueValue(evt, convertClosure)
+    if (settings.queueTime.toInteger() > 0) {
+        queueValue(evt, convertClosure)
     } else {
-		sendValue(evt, convertClosure)
+        sendValue(evt, convertClosure)
     }
 }
 
 def handleLockEvent(evt) {
-	// default to locked/unlocked, the value of the event
+    // default to locked/unlocked, the value of the event
     def convertClosure = { it }
     if (lockLogType == "true/false") {
-		convertClosure = { it == "locked" ? "true" : "false" }
+        convertClosure = { it == "locked" ? "true" : "false" }
     }else if (lockLogType == "1/0") {
-    	convertClosure = { it == "locked" ? "1" : "0" }
-	}
-	if(settings.queueTime.toInteger() > 0) {
-    	queueValue(evt, convertClosure)
+        convertClosure = { it == "locked" ? "1" : "0" }
+    }
+    if (settings.queueTime.toInteger() > 0) {
+        queueValue(evt, convertClosure)
     } else {
-		sendValue(evt, convertClosure)
+        sendValue(evt, convertClosure)
     }
 }
 
 
 private sendValue(evt, Closure convert) {
-	def keyId = URLEncoder.encode(evt.displayName.trim()+ " " +evt.name)
-	def value = URLEncoder.encode(convert(evt.value))
+    def keyId = URLEncoder.encode(evt.displayName.trim()+ " " +evt.name)
+    def value = URLEncoder.encode(convert(evt.value))
 
-	log.debug "Logging to GoogleSheets ${keyId} = ${value}"
+    log.debug "Logging to GoogleSheets ${keyId} = ${value}"
     
-	def url = "https://script.google.com/macros/s/${urlKey}/exec?${keyId}=${value}"
+    def url = "https://script.google.com/macros/s/${urlKey}/exec?${keyId}=${value}"
     log.debug "${url}"
     
-	def putParams = [
-		uri: url]
+    def putParams = [
+        uri: url]
 
-	httpGet(putParams) { response ->
-    	log.debug(response.status)
-		if (response.status != 200 ) {
-			log.debug "Google logging failed, status = ${response.status}"
-		}
-	}
+    httpGet(putParams) { response ->
+        log.debug(response.status)
+        if (response.status != 200 ) {
+            log.debug "Google logging failed, status = ${response.status}"
+        }
+    }
 }
 
 private queueValue(evt, Closure convert) {
-	checkAndProcessQueue()
-	if( evt?.value ) {
-    	
-    	def keyId = URLEncoder.encode(evt.displayName.trim()+ " " +evt.name)
-		def value = URLEncoder.encode(convert(evt.value))
-    
-    	log.debug "Logging to queue ${keyId} = ${value}"
+    checkAndProcessQueue()
+    if ( evt?.value ) {
         
-		if( atomicState.queue == [:] ) {
-      		def eventTime = URLEncoder.encode(evt.date.format( 'yyyy-M-d HH:mm:ss', location.timeZone ))
+        def keyId = URLEncoder.encode(evt.displayName.trim()+ " " +evt.name)
+        def value = URLEncoder.encode(convert(evt.value))
+    
+        log.debug "Logging to queue ${keyId} = ${value}"
+        
+        if ( atomicState.queue == [:] ) {
+            def eventTime = URLEncoder.encode(evt.date.format( 'yyyy-M-d HH:mm:ss', location.timeZone ))
             addToQueue("Time", eventTime)
-    	}
-		addToQueue(keyId, value)
+        }
+        addToQueue(keyId, value)
         
         log.debug(atomicState.queue)
 
-    	scheduleQueue()
-	}
+        scheduleQueue()
+    }
 }
 
 /*
  * atomicState acts differently from state, so we have to get the map, put the new item and copy the map back to the atomicState
  */
 private addToQueue(key, value) {
-	def queue = atomicState.queue
-	queue.put(key, value)
-	atomicState.queue = queue
+    def queue = atomicState.queue
+    queue.put(key, value)
+    atomicState.queue = queue
 }
 
 private checkAndProcessQueue() {
     if (atomicState.scheduled && ((now() - atomicState.lastSchedule) > (settings.queueTime.toInteger()*120000))) {
-		// if event has been queued for twice the amount of time it should be, then we are probably stuck
+        // if event has been queued for twice the amount of time it should be, then we are probably stuck
         sendEvent(name: "scheduleFailure", value: now())
         unschedule()
-    	processQueue()
+        processQueue()
     }
 }
 
 def scheduleQueue() {
-	if(atomicState.failureCount >= 3) {
-	    log.debug "Too many failures, clearing queue"
+    if (atomicState.failureCount >= 3) {
+        log.debug "Too many failures, clearing queue"
         sendEvent(name: "queueFailure", value: now())
         resetState()
     }
-	
-    if(!atomicState.scheduled) {
-    	runIn(settings.queueTime.toInteger() * 60, processQueue)
+    
+    if (!atomicState.scheduled) {
+        runIn(settings.queueTime.toInteger() * 60, processQueue)
         atomicState.scheduled=true
         atomicState.lastSchedule=now()
     } 
